32c1bcab7c199d4991040b2954b65fe18a375274
nmosman2002@gmail.com <nmosman2002@gmail.com>
2023-06-22 21:43:50 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -5,5 +5,6 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.label_1.text = "Hello World"
 
     # Any code you write here will run before the form opens.
