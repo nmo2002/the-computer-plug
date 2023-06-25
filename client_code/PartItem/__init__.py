9c9fba3d1641c09c8f8721ce97c598e6160b84d5
nmosman2002@gmail.com <nmosman2002@gmail.com>
2023-06-25 17:26:22 +0000

    Edited settings, Parts and PartItem

--- client_code/PartItem/__init__.py
+++ client_code/PartItem/__init__.py
@@ -9,12 +9,13 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 class PartItem(PartItemTemplate):
-  def __init__(self, name, description, button_text, **properties):
+  def __init__(self, name, description, button_text, image, button_callback, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.name_label.text = name
-    self.description_label.text = description
+    self.name_label.content = name
+    self.description_label.content = description
     self.button.text = button_text
+    self.image_content.source = image
 
     # Any code you write here will run before the form opens.
 
--- client_code/Parts/__init__.py
+++ client_code/Parts/__init__.py
@@ -15,12 +15,14 @@ class Parts(PartsTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.load_parts()
-    p = PartItem(name = 'NVIDIA GeForce RTX 4090', button_text = 'Buy for #1599.99', description = 'Graphics card')
-    self.content_panel.add_component(p)
+    
 
   def load_parts(self):
     parts = anvil.server.call('get_part_details').search()
+    course_panel = GridPanel()
     
-    for part in parts:
-      print(part['name'])
+    for i, part in enumerate(parts):
+      p = PartItem(name=part['name'], button_text = f'Buy for ${part["price"]}', description=part['description'], image=part['image'], button_callback=None)
+      course_panel.add_component(p, row=str(i//2), width_xs=6)
     # Any code you write here will run before the form opens.
+    self.content_panel.add_component(course_panel)
