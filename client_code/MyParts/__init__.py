8143c17b1eaeaa05c729c0b2cb0e60a4b9e9b50e
nmosman2002@gmail.com <nmosman2002@gmail.com>
2023-07-01 19:12:09 +0000

    Edited MyParts

--- client_code/MyParts/__init__.py
+++ client_code/MyParts/__init__.py
@@ -8,19 +8,23 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.users
+from ..PartItem import PartItem
 
 class MyParts(MyPartsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.load_parts()
-
+  
+  def render_part(self, part_name):
+    self.content_panel.clear()
+    
   def load_parts(self):
-    parts = anvil.server.call('get_my_parts').search()
+    parts = anvil.server.call('get_my_parts')
     part_panel = GridPanel()
       
     for i, part in enumerate(parts):
-      p = PartItem(name=part['name'], button_text = f'Buy for ${part["price"]}', description=part['description'], image=part['image'], button_callback=self.render_checkout)
+      p = PartItem(name=part['name'], button_text = 'View Content', description=part['description'], image=part['image'], button_callback=self.render_part)
       part_panel.add_component(p, row=str(i//2), width_xs=6)
       # Any code you write here will run before the form opens.
     self.content_panel.add_component(part_panel)
